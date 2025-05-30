18d852577bb17e555195d0a49074d030e15b0284
AMMH11 <117871852+AMMH11@users.noreply.github.com>
2025-05-30 20:44:35 -0300

    Update db.py

--- FastApi/recom_api/app/db.py
+++ FastApi/recom_api/app/db.py
@@ -1,12 +1,14 @@
-#contraseñas fake porque las reales son otras.
-
+#para corregir el punto levantado de seguridad en la exposicion de contraseñas dejamos un file generico
+#y las reales estan en un archivo.env que no subimos al repositorio por motivos de seguridad.
+import os
 import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",         
-        dbname="Postgres",
-        user="Postgres",
-        password="admin123",
-        port=5432
+        host=os.getenv("DB_HOST"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
     )
+
